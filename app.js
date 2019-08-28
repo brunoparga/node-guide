94869289abcc8555bca9a3bde75a79a91b906592
Bruno Parga <brunoparga@gmail.com>
2019-08-28 17:01:00 -0300

    Install Express framework

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// Core Node modules
 const http = require('http')
 
-const routes = require('./routes')
+// Third-party packages
+const express = require('express')
 
-const server = http.createServer(routes)
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('In the middleware!')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('In another middleware')
+})
+
+const server = http.createServer(app)
 
 server.listen(3000)
