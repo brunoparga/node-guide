c4a3b29e3f119cd5fb63405fb6cf12ca3302b6cb
Bruno Parga <brunoparga@gmail.com>
2019-08-30 22:18:15 -0300

    Solve Assignment 2

---
+++ assignment-2/app.js
@@ -0,0 +1,17 @@
+// Core Node modules
+// Third-party packages
+const express = require('express')
+
+// My own requires
+// Module
+const app = express()
+
+app.use('/users', (req, res, next) => {
+  res.send('<ul><li>User1</li><li>User2</li></ul>')
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>HELLO WORLD!!1!eleven!</h1>')
+})
+
+app.listen(3000)
