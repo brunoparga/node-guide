668be6fc89431e60f30a01205e8218d9f695f8c7
Bruno Parga <brunoparga@gmail.com>
2019-08-30 22:18:01 -0300

    Fix style with Standard

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const express = require('express')
 const app = express()
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>The "Add Product" page</h1>')
+  console.log('In another middleware')
+  res.send('<h1>The "Add Product" page</h1>')
 })
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Hello from Express!</h1>')
+  console.log('In another middleware')
+  res.send('<h1>Hello from Express!</h1>')
 })
 
 app.listen(3000)
