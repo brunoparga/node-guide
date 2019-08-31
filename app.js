ad81a30af65e1a66ce187730ab1cc2b2ab8ce99c
Bruno Parga <brunoparga@gmail.com>
2019-08-30 22:07:17 -0300

    Finish section 5.1

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// Core Node modules
-const http = require('http')
+// Requiring core Node.js modules
 
-// Third-party packages
+// Requiring third-party packages
 const express = require('express')
 
+// Requiring our own code
+
+// Our code for this module
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middleware!')
-    next()
-})
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware')
+    res.send('<h1>The "Add Product" page</h1>')
 })
 
-const server = http.createServer(app)
+app.use('/', (req, res, next) => {
+    console.log('In another middleware')
+    res.send('<h1>Hello from Express!</h1>')
+})
 
-server.listen(3000)
+app.listen(3000)
