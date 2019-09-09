49538e8add9d8e558f3d8106f100f912799e2e46
Bruno Parga <brunoparga@gmail.com>
2019-09-09 15:20:56 -0300

    Comment the main file so I know I've learned

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
+// IMPORTS SECTION
+// Node core module to deal with file paths
 const path = require('path')
 
+// NPM packages
+// The express.js framework
 const express = require('express')
+// Parses the body of a POST request
 const bodyParser = require('body-parser')
 
+// App routes have been segregated into different files
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+// =================
+// =================
+
+// Main app object
 const app = express()
 
+// All middleware functions
+
+// Parse request body
 app.use(bodyParser.urlencoded({ extended: true }))
+// Serve static files, like CSS and browser JS
 app.use(express.static(path.join(__dirname, 'public')))
-
+// Prepend a path to these routes
 app.use('/admin', adminRoutes)
+// But not these
 app.use(shopRoutes)
-
+// Fall back to sending a 404
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
+// App listens on port 3000
 app.listen(3000)
