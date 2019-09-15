3c4c69924109fd0483f8089c9d50b737a17e1234
Bruno Parga <brunoparga@gmail.com>
2019-09-15 18:01:37 -0300

    Fix style of an object

--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,12 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   // fetch data from adminData and inject it into view
   const products = adminData.products
-  res.render('shop', { prods: products,
-                       pageTitle: 'Shop',
-                       path: '/',
-                       hasProducts: products.length > 0 })
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0
+  })
 })
 
 module.exports = router
