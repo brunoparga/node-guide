28c414cdc78fa1f51f91abea4586b7e3f95d8f49
Bruno Parga <brunoparga@gmail.com>
2019-09-15 14:18:47 -0300

    Convert each of the 3 views to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,10 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   // fetch data from adminData and inject it into view
   const products = adminData.products
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
+  res.render('shop', { prods: products,
+                       pageTitle: 'Shop',
+                       path: '/',
+                       hasProducts: products.length > 0 })
 })
 
 module.exports = router
