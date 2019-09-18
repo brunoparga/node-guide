0a9e74cdbd277a2ee20419f4f6ce82523d03841c
Bruno Parga <brunoparga@gmail.com>
2019-09-18 18:11:55 -0300

    Remove main file comments ('training wheels')

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-// IMPORTS SECTION
-// Node core module to deal with file paths
 const path = require('path')
-
-// NPM packages
-// The express.js framework
 const express = require('express')
-// Parses the body of a POST request
 const bodyParser = require('body-parser')
 
-// =================
-// =================
-
-// Main app object
 const app = express()
 app.set('view engine', 'ejs')
-// Configure views folder (not needed, as this is the default)
-app.set('views', 'views')
 
-// App routes have been segregated into different files
 const adminRouter = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const pagesController = require('./controllers/pages')
-
-// All middleware functions
 
 // Parse request body
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -35,5 +20,4 @@ app.use(shopRoutes)
 // Fall back to sending a 404
 app.use(pagesController.get404)
 
-// App listens on port 3000
 app.listen(3000)
