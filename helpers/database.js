2955452bbfa24929398d0eee2ddd9da78f8ba39b
Bruno Parga <brunoparga@gmail.com>
2019-09-30 18:20:26 -0300

    Set up Sequelize to improve SQL handling

--- helpers/database.js
+++ helpers/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node_complete',
-  password: 'password',
-});
+const sequelize = new Sequelize(
+  'node_complete',
+  'root',
+  'password',
+  {
+    dialect: 'mysql',
+    host: 'localhost',
+  },
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
