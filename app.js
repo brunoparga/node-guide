34f0965e435b80a023781379b217874d294029a8
Bruno Parga <brunoparga@gmail.com>
2019-10-03 14:37:22 -0300

    Set up Order model

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
-
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // Import routes
 const adminRouter = require('./routes/admin');
@@ -44,6 +45,10 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
 
 // Sync DB and start up app
 sequelize.sync()
