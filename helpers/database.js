d6ff6475cb079c6333022931b3ec35e1f73107a6
Bruno Parga <brunoparga@gmail.com>
2019-10-03 19:10:04 -0300

    Begin replacing MySQL with MongoDB

--- helpers/database.js
+++ helpers/database.js
@@ -1,13 +1,17 @@
-const Sequelize = require('sequelize');
+const { MongoClient } = require('mongodb');
 
-const sequelize = new Sequelize(
-  'node_complete',
-  'root',
-  'password',
-  {
-    dialect: 'mysql',
-    host: 'localhost',
-  },
-);
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://brunoparga:5KJMQ5F97T5XUNMGKB65L8DKX53BB@cluster0-jhvlt.mongodb.net/admin?retryWrites=true&w=majority',
+    // Configuration required by deprecation warnings
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+    .then((client) => {
+      callback(client);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
