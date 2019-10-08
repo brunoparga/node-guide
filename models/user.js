b96d0d1c3543e701946905744085467c4b9ca2d8
Bruno Parga <brunoparga@gmail.com>
2019-10-07 22:47:51 -0300

    Include Cart as embedded document of User

--- models/user.js
+++ models/user.js
@@ -2,13 +2,27 @@ const { ObjectId } = require('mongodb');
 const { getDb } = require('../helpers/database');
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, _id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = _id;
   }
 
   save() {
     return getDb().collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => cp._id === product._id)
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    // this.cart = updatedCart;
+    return getDb()
+      .collection('users')
+      .updateOne(
+        { _id: ObjectId(this._id) },
+        { $set: { cart: updatedCart } },
+      );
   }
 
   static findById(userId) {
