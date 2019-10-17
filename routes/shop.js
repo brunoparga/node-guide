8e0d738c74b555244d3d540337d4bda1f69a1336
Bruno Parga <brunoparga@gmail.com>
2019-10-17 15:12:33 -0300

    Display orders from database

--- routes/shop.js
+++ routes/shop.js
@@ -8,6 +8,6 @@ router.post('/cart', shopController.postCart);
 router.get('/cart', shopController.getCart);
 router.post('/cart-delete-item', shopController.postDeleteItem);
 router.post('/create-order', shopController.postOrder);
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
