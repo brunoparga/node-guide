b166404a13556ed464d1de81cfd259ffa998d5af
Bruno Parga <brunoparga@gmail.com>
2019-10-22 10:09:33 -0300

    Implement password recovery

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const mailer = require('@sendgrid/mail');
 
@@ -72,6 +73,101 @@ exports.postLogin = (req, res) => {
           return res.redirect('/login');
         });
     });
+};
+
+exports.getReset = (req, res) => {
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset password',
+    errorMessage: req.flash('error')[0],
+  });
+};
+
+exports.postReset = (req, res) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    return User
+      .findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'Email not found.');
+          return res.redirect('/reset');
+        }
+        const newUser = user;
+        newUser.resetToken = token;
+        newUser.resetTokenExpiration = Date.now() + 3600000;
+        return newUser.save();
+      })
+      .then(() => {
+        res.redirect('/');
+        mailer.send({
+          to: req.body.email,
+          from: 'recovery@superstore.com',
+          subject: 'Recover your password',
+          html: `
+            <p>Someone (likely you) requested a password reset for this email.</p>
+            <p>Click <a href="http://localhost:3000/new-password/${token}">this link</a> to reset your password:</p>
+
+            Cheers,<br>
+            SuperStore.com
+          `,
+        });
+      });
+  });
+};
+
+exports.getNewPassword = (req, res) => {
+  const { token } = req.params;
+  User
+    .findOne({
+      resetToken: token,
+      resetTokenExpiration: { $gt: Date.now() },
+    })
+    .then((user) => {
+      if (!user) {
+        req.flash('error', 'Invalid recovery token.');
+        res.redirect('/');
+      } else {
+        res.render('auth/new-password', {
+          path: '/new-password',
+          pageTitle: 'New Password',
+          errorMessage: req.flash('error')[0],
+          _id: user._id.toString(),
+          resetToken: token,
+        });
+      }
+    });
+};
+
+exports.postNewPassword = (req, res) => {
+  const {
+    _id, newPassword, confirmPassword, resetToken,
+  } = req.body;
+  if (newPassword !== confirmPassword) {
+    req.flash('error', "The passwords don't match.");
+    return res.redirect('/signup');
+  }
+  let updatedUser;
+  return User
+    .findOne({
+      _id,
+      resetToken,
+      resetTokenExpiration: { $gt: Date.now() },
+    })
+    .then((user) => {
+      updatedUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      updatedUser.password = hashedPassword;
+      updatedUser.resetToken = undefined;
+      updatedUser.resetTokenExpiration = undefined;
+      return updatedUser.save();
+    })
+    .then(() => res.redirect('/login'));
 };
 
 exports.postLogout = (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
+router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
+router.get('/new-password/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
 router.post('/logout', authController.postLogout);
 
 module.exports = router;
