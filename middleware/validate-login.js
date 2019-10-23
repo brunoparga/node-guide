1a45ae589aa7553d9eea1c639551ad13fb1def7b
Bruno Parga <brunoparga@gmail.com>
2019-10-22 22:17:54 -0300

    Sanitize user data

--- middleware/validate-login.js
+++ middleware/validate-login.js
@@ -1,7 +1,9 @@
 const { body } = require('express-validator');
 
 module.exports = [
-  body('email', 'Invalid email address').isEmail(),
+  body('email', 'Invalid email address').isEmail().normalizeEmail(),
   body('password', 'Password must be at least 4 characters long.')
-    .isLength({ min: 4, max: 128 }),
+    .isLength({ min: 4, max: 128 })
+    .trim(),
+  body('confirmPassword').trim(),
 ];
