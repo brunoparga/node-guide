510470f46d27b488fa60688ac757a6c9c0a5c293
Bruno Parga <brunoparga@gmail.com>
2019-10-23 22:39:29 -0300

    Handle errors in setUser middleware

--- middleware/set-user.js
+++ middleware/set-user.js
@@ -2,11 +2,9 @@ const User = require('../models/user');
 
 module.exports = (req, _res, next) => {
   if (req.session.user) {
-    User.findById(req.session.user._id).then((user) => {
-      req.user = user;
-      next();
-    });
-  } else {
-    next();
-  }
+    User.findById(req.session.user._id)
+      .then((user) => { if (user) { req.user = user; } })
+      .then(() => next())
+      .catch((err) => { throw new Error(err); });
+  } else { next(); }
 };
