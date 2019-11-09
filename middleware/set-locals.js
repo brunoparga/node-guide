078c84fbf68d659682fa44c5f19a28703037100a
Bruno Parga <brunoparga@gmail.com>
2019-11-08 22:29:41 -0300

    DRY out product cards with an include

--- middleware/set-locals.js
+++ middleware/set-locals.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.locals.isAuthenticated = req.user;
+  res.locals.isAuthenticated = req.user !== undefined;
   res.locals.csrfToken = req.csrfToken();
   if (req.user) {
     res.locals.userEmail = req.user.email;
